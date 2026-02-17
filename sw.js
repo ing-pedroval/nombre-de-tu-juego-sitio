1d26f16b98d4090dedb0e65c3e8b317babd91b17
Pedro Valenzuela <pedro.ozvg@outlook.com>
2026-02-17 15:35:19 -0600

    Actualizacion de assets, pwa, con manifest y service worker

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'site-static-v1';
+
+const assets = [
+  '/',
+  './index.html',
+  '/script.js',
+  '/styles.css',
+  '/manifest.json',
+  '/icon-192.jpg',
+  '/icon-512.jpg'
+];
+
+// Se instala el service worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching shell assets');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activate event (runs when the SW is updated)
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event (Interacts with the cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      // Return cached file or go to network
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
